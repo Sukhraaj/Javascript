a7b7e83946295796c5ddd95225ce51ca568a1d47
Sukhraaj <101177444+Sukhraaj@users.noreply.github.com>
2024-08-08 13:14:26 +0000

    dataypes summary add ons

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -2,9 +2,33 @@
 
 // 7 types : String, Number, Boolen, null, undefined, symbol, BigInt
 
+const  score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let useEmail;
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)
+
+ const bigNumber = 4235343427897n
 
 
 
 // Refernce (Non Primitive)
 
 // Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+
+let myObj = {
+    names: "Sukhe",
+    age:22,
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
+console.log(typeof bigNumber);
