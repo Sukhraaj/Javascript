445533d0ac98496b0597b3dffad0b60092eba108
Sukhraaj <101177444+Sukhraaj@users.noreply.github.com>
2024-08-14 15:15:51 +0000

    String_done

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,34 @@
+const name = "Sukhraaj"
+const repoCount = 8
+
+// console.log(name + repoCount + "Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount} `);
+
+const gameName = new String("Sukhe")
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf("e"));
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+ 
+const anotherString = gameName.slice(-3,4)
+console.log(anotherString);
+
+const newStringONe = "   Sukhraaj    "
+console.log(newStringONe);
+console.log(newStringONe.trim());
+
+const url = "https://Sukhraaj.com/sukhraaj%20Singh"
+
+console.log(url.replace('%20','-'));
+console.log(url.includes('sukh'));
+
+console.log(gameName.split('k'));
+
